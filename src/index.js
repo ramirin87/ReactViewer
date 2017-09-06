be37c65b923268479e2ed7d05f980217d1016bff
Elmer <erga1987@gmail.com>
2017-09-06 05:48:15 -0600

    archivos

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyBq-_P3Se6cMe1d68HN7Tm1CYv0fXV3L_A';
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+
+        YTSearch({key:API_KEY, term:'real'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />                
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
